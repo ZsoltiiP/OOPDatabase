293378c1180b115d36553077a05c090801eee383
poraczkizs <poraczkizs@kkszki.hu>
2025-09-22 08:09:08 +0200

    Interface and tablebooks class are added

--- OOPAdatbazis/Program.cs
+++ OOPAdatbazis/Program.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OOPDatabase;
+using System;
 
-namespace OOPAdatbazis
+namespace OOPDatabase
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             Console.Write("Kérem az adatbázis nevét: ");
             string dbName = Console.ReadLine();
 
-            Console.Write("Kérem az felhasználó nevét: ");
+            Console.Write("Kérem a felhasználó nevét: ");
             string userName = Console.ReadLine();
-            
-            Console.Write("Kérem az felhasználó jelszavát: ");
+
+            Console.Write("Kérem a felhasználó jelszavát: ");
             string userPass = Console.ReadLine();
 
             Connect c = new Connect(dbName, userName, userPass);
